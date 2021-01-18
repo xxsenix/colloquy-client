1e254b37bc9e385246401bc222992c64d60f7067
Travis Callahan <callahantravis29@gmail.com>
2021-01-18 14:08:36 -0600

    Changed footer to 2021

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import './footer.css';
+import React from "react";
+import "./footer.css";
 
 export default function Footer(props) {
-    
-    return (
-        <footer className="footer">
-            <p className="footer-text">© 2019 Colloquy. Created by Travis Callahan</p>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <p className="footer-text">© 2021 Colloquy. Created by Travis Callahan</p>
+    </footer>
+  );
 }
-
