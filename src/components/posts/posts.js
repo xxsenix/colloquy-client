70c082353fd68e94e841e49bee87e7f9ed12da6f
Travis Callahan <callahantravis29@gmail.com>
2021-01-18 14:38:19 -0600

    Updated Posts component.

--- src/components/posts/posts.js
+++ src/components/posts/posts.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { getAllPosts } from "../../actions/posts";
 import Post from "./post";
 import Spinner from "react-spinkit";
 import "./posts.css";
 
-export class Posts extends React.Component {
-  isLoading() {
-    if (this.props.loading) {
+const Posts = (props) => {
+  const isLoading = () => {
+    if (props.loading) {
       return (
         <Spinner
           name="three-bounce"
@@ -15,54 +15,118 @@ export class Posts extends React.Component {
             display: "flex",
             color: "#ffffff",
             justifyContent: "center",
-            margin: "40px"
+            margin: "40px",
           }}
         />
       );
     }
-  }
+  };
 
-  componentDidMount() {
-    this.props.dispatch(getAllPosts());
-  }
+  useEffect(() => {
+    props.dispatch(getAllPosts());
+  }, []);
 
-  render() {
-    let posts;
+  let posts;
 
-    if (
-      this.props.match !== undefined &&
-      this.props.match.params.category !== undefined
-    ) {
-      posts = this.props.posts.filter(
-        post => post.category.toLowerCase() === this.props.match.params.category
-      );
-    } else if (
-      this.props.match !== undefined &&
-      this.props.match.params.user !== undefined
-    ) {
-      posts = this.props.posts.filter(
-        post => post.author.toLowerCase() === this.props.match.params.user
-      );
-    } else {
-      posts = this.props.posts;
-    }
-
-    return (
-      <section className="posts">
-        {this.isLoading()}
-        <ul className="posts-list">
-          <Post posts={posts} />
-        </ul>
-      </section>
+  if (props.match !== undefined && props.match.params.category !== undefined) {
+    posts = props.posts.filter(
+      (post) => post.category.toLowerCase() === props.match.params.category
     );
+  } else if (
+    props.match !== undefined &&
+    props.match.params.user !== undefined
+  ) {
+    posts = props.posts.filter(
+      (post) => post.author.toLowerCase() === props.match.params.user
+    );
+  } else {
+    posts = props.posts;
   }
-}
 
-const mapStateToProps = state => {
+  return (
+    <section className="posts">
+      {isLoading()}
+      <ul className="posts-list">
+        <Post posts={posts} />
+      </ul>
+    </section>
+  );
+};
+
+const mapStateToProps = (state) => {
   return {
     posts: state.posts.posts,
-    loading: state.posts.loading
+    loading: state.posts.loading,
   };
 };
 
 export default connect(mapStateToProps)(Posts);
+
+// import React from "react";
+// import { connect } from "react-redux";
+// import { getAllPosts } from "../../actions/posts";
+// import Post from "./post";
+// import Spinner from "react-spinkit";
+// import "./posts.css";
+
+// export class Posts extends React.Component {
+//   isLoading() {
+//     if (this.props.loading) {
+//       return (
+//         <Spinner
+//           name="three-bounce"
+//           style={{
+//             display: "flex",
+//             color: "#ffffff",
+//             justifyContent: "center",
+//             margin: "40px"
+//           }}
+//         />
+//       );
+//     }
+//   }
+
+//   componentDidMount() {
+//     this.props.dispatch(getAllPosts());
+//   }
+
+//   render() {
+//     let posts;
+
+//     if (
+//       this.props.match !== undefined &&
+//       this.props.match.params.category !== undefined
+//     ) {
+//       posts = this.props.posts.filter(
+//         post => post.category.toLowerCase() === this.props.match.params.category
+//       );
+//     } else if (
+//       this.props.match !== undefined &&
+//       this.props.match.params.user !== undefined
+//     ) {
+//       posts = this.props.posts.filter(
+//         post => post.author.toLowerCase() === this.props.match.params.user
+//       );
+//     } else {
+//       posts = this.props.posts;
+//     }
+
+//     return (
+//       <section className="posts">
+//         {this.isLoading()}
+//         <ul className="posts-list">
+//           <Post posts={posts} />
+//         </ul>
+//       </section>
+//     );
+//   }
+// }
+
+// const mapStateToProps = state => {
+//   return {
+//     posts: state.posts.posts,
+//     loading: state.posts.loading
+//   };
+// };
+
+// export default connect(mapStateToProps)(Posts);
